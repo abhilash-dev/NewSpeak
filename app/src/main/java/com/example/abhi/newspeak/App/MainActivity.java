39a2597fd38c631e88401a9969c94e9e3565b8fe
Abhilash S <abhilash1869@gmail.com>
2016-08-24 21:39:09 +0530

    Added Login & register Activities

---
+++ app/src/main/java/com/example/abhi/newspeak/App/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.abhi.newspeak.App;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.transition.Explode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.abhi.newspeak.NyTimes.FetchData;
+import com.example.abhi.newspeak.R;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.et_username)
+    EditText etUsername;
+    @InjectView(R.id.et_password)
+    EditText etPassword;
+    @InjectView(R.id.bt_go)
+    Button btGo;
+    @InjectView(R.id.cv)
+    CardView cv;
+    @InjectView(R.id.fab)
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        // Create default options which will be used for every
+        //  displayImage(...) call if no options will be passed to this method
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+
+                .build();
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
+
+                .defaultDisplayImageOptions(defaultOptions)
+
+                .build();
+        ImageLoader.getInstance().init(config); // Do it on Application start
+
+    }
+
+    @OnClick({R.id.bt_go, R.id.fab})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.fab:
+                getWindow().setExitTransition(null);
+                getWindow().setEnterTransition(null);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options =
+                            ActivityOptions.makeSceneTransitionAnimation(this, fab, fab.getTransitionName());
+                    startActivity(new Intent(this, RegisterActivity.class), options.toBundle());
+                } else {
+                    startActivity(new Intent(this, RegisterActivity.class));
+                }
+                break;
+            case R.id.bt_go:
+                Explode explode = new Explode();
+                explode.setDuration(500);
+
+                getWindow().setExitTransition(explode);
+                getWindow().setEnterTransition(explode);
+                ActivityOptionsCompat oc2 = ActivityOptionsCompat.makeSceneTransitionAnimation(this);
+                Intent i2 = new Intent(this, FetchData.class);
+                startActivity(i2, oc2.toBundle());
+                break;
+        }
+    }
+}
+
